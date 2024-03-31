cb7babe17239570f39bc97d63acd7e0c02df4490
laptop_mothership <cv.to@abv.bg>
2024-03-31 14:59:08 +0300

    Added migrations.

---
+++ HouseRentingSystem.Infrastructure/Migrations/20240331115116_AddedEntitiesFor_Agent_House_Category.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HouseRentingSystem.Infrastructure.Migrations
+{
+    public partial class AddedEntitiesFor_Agent_House_Category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Unique identifier of the Agent")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false, comment: "Phone of the Agent"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "User Id of the Agent")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agents_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Category unique identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Category's name")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Houses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "House unique identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "House Title"),
+                    Address = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false, comment: "House Address"),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false, comment: "House Description"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Url of the House's image"),
+                    PricePerMonth = table.Column<decimal>(type: "decimal(18,2)", nullable: false, comment: "House price per month"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false, comment: "Id of the House's Category"),
+                    AgentId = table.Column<int>(type: "int", nullable: false, comment: "Id of the House's Agent"),
+                    RenterId = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Id of the House's renter")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Houses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Houses_Agents_AgentId",
+                        column: x => x.AgentId,
+                        principalTable: "Agents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Houses_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agents_UserId",
+                table: "Agents",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Houses_AgentId",
+                table: "Houses",
+                column: "AgentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Houses_CategoryId",
+                table: "Houses",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Houses");
+
+            migrationBuilder.DropTable(
+                name: "Agents");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
