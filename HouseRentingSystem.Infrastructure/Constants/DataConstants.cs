3d8201f5149db8ef06d6ce6e5f463fc3696998c5
laptop_mothership <cv.to@abv.bg>
2024-03-31 14:55:36 +0300

    Added a static data constants class

---
+++ HouseRentingSystem.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace HouseRentingSystem.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int CategoryMaxLength = 50;
+
+        public const int HouseTitleMinLength = 10;
+        public const int HouseTitleMaxLength = 50;
+
+        public const int HouseAddressMinLength = 30;
+        public const int HouseAddressMaxLength = 150;
+
+        public const int HouseDescMinLength = 50;
+        public const int HouseDescMaxLength = 500;
+
+        public const string HousePricePerMonthMin = "0.00";
+        public const string HousePricePerMonthMax = "2000.00";
+
+        public const int AgentPhoneMaxLength = 7;
+        public const int AgentPhoneMinLength = 15;
+    }
+}
