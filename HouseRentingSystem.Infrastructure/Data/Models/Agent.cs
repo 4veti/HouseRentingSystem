f1d9e11791b38900aab265b2e5edea07a7737c13
laptop_mothership <cv.to@abv.bg>
2024-03-31 14:57:13 +0300

    Created the entity classes for Agent, Category, and House.

---
+++ HouseRentingSystem.Infrastructure/Data/Models/Agent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    public class Agent
+    {
+        /// <summary>
+        /// Agent's unique identifier
+        /// </summary>
+        [Key]
+        [Comment("Unique identifier of the Agent")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Agent's phone number
+        /// </summary>
+        [Required]
+        [MaxLength(AgentPhoneMaxLength)]
+        [Comment("Phone of the Agent")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// User identifier of the Agent
+        /// </summary>
+        [Required]
+        [Comment("User Id of the Agent")]
+        public string UserId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Identity user of the Agent
+        /// </summary>
+        public IdentityUser User { get; set; } = null!;
+    }
+}
+
+//· Id – a unique integer, Primary Key
+//· PhoneNumber – a string with min length 7 and max length 15 (required)
+//· UserId – a string (required)
+//· User – an IdentityUser object
---
+++ HouseRentingSystem.Infrastructure/Data/Models/House.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    public class House
+    {
+        /// <summary>
+        /// Unique identifier of the House entity class
+        /// </summary>
+        [Key]
+        [Comment("House unique identifier")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of the House
+        /// </summary>
+        [Required]
+        [MaxLength(HouseTitleMaxLength)]
+        [Comment("House Title")]
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Address of the House
+        /// </summary>
+        [Required]
+        [MaxLength(HouseAddressMaxLength)]
+        [Comment("House Address")]
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Description of the House
+        /// </summary>
+        [Required]
+        [MaxLength(HouseDescMaxLength)]
+        [Comment("House Description")]
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Url that links to the House's image
+        /// </summary>
+        [Required]
+        [Comment("Url of the House's image")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Price per month of the House
+        /// </summary>
+        [Required]
+        //[Range(typeof(decimal), HousePricePerMonthMin, HousePricePerMonthMax, ConvertValueInInvariantCulture = true)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Comment("House price per month")]
+        public decimal PricePerMonth { get; set; }
+
+        /// <summary>
+        /// Id of the House's Category
+        /// </summary>
+        [Required]
+        [Comment("Id of the House's Category")]
+        public int CategoryId { get; set; }
+        /// <summary>
+        /// Navigation property to the House's Category
+        /// </summary>
+        public Category Category { get; set; } = null!;
+
+        /// <summary>
+        /// Id of the House's Agent
+        /// </summary>
+        [Required]
+        [Comment("Id of the House's Agent")]
+        public int AgentId { get; set; }
+        /// <summary>
+        /// Navigation property to the House's Agent
+        /// </summary>
+        public Agent Agent { get; set; } = null!;
+
+        /// <summary>
+        /// Id of the House's renter
+        /// </summary>
+        [Comment("Id of the House's renter")]
+        public string? RenterId { get; set; }
+    }
+}
