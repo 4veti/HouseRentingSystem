796a7b9fdb4070469cbf257565df93be9640c4cd
laptop_mothership <cv.to@abv.bg>
2024-04-02 01:05:12 +0300

    Changed the Home controller, added its views and models.

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using HouseRentingSystem.Models;
+using HouseRentingSystem.Models.Home;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HouseRentingSystem.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -13,11 +15,13 @@ namespace HouseRentingSystem.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            return View(new IndexViewModel());
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
