0b25d371209b603d7832d020e10fb137239de93d
laptop_mothership <cv.to@abv.bg>
2024-04-04 23:37:01 +0300

    Added Unit of Work class.

---
+++ HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Infrastructure.Data.Contracts
+{
+    public interface IRepository 
+    {
+        public IQueryable<T> AllAsync<T>() where T : class; 
+        public IQueryable<T> AllAsReadOnlyAsync<T>() where T : class;
+    }
+}
---
+++ HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private DbContext context;
+
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public IQueryable<T> AllAsync<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllAsReadOnlyAsync<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+            => context.Set<T>();
+    }
+
+}
