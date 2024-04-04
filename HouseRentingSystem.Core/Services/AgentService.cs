8a03e7fd7f05ca1a9c6133a8c4ba2127cca79c90
laptop_mothership <cv.to@abv.bg>
2024-04-04 23:58:37 +0300

    Added Agent service.

---
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -0,0 +1,20 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Infrastructure.Data.Contracts;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+        private IRepository repository;
+
+        public AgentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public Task<bool> ExistsById(string userId)
+            => repository.AllAsReadOnlyAsync<Agent>().AnyAsync(a => a.UserId == userId);
+    }
+}
